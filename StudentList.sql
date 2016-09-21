340890d2edb6f3421835573ebe3f4efb5e3a8424
Ankeet Shah <ashah@summitps.org>
2016-09-20 17:55:53 -0700

    Add mentors
    Add mentors

--- StudentList.sql
+++ StudentList.sql
@@ -1,7 +1,7 @@
 /*
 
 Summary
-• Pulls a list of currently enrolled students
+• Pulls a list of students that are currently enrolled in at least one class
 
 Level of Detail
 • student
@@ -9,24 +9,39 @@ Level of Detail
 */
 
 SELECT DISTINCT
-    TRIM(site_name) AS "Site"
-  , short_name::INTEGER AS "Grade Level"
-  , local_student_id AS "Student ID"
-  , student_id AS "Illuminate Student ID"
-  , TRIM(last_name) AS "Student Last Name"
-  , TRIM(first_name) AS "Student First Name"
-  , TRIM(middle_name) AS "Student Middle Name"
-  , LOWER(email) AS "Student Email"
+    TRIM(sites.site_name) AS "Site"
+  , grade_levels.short_name::INTEGER AS "Grade Level"
+  , students.local_student_id AS "Student ID"
+  , students.student_id AS "Illuminate Student ID"
+  , TRIM(students.last_name) AS "Student Last Name"
+  , TRIM(students.first_name) AS "Student First Name"
+  , TRIM(students.middle_name) AS "Student Middle Name"
+  , LOWER(students.email) AS "Student Email"
+  , users.last_name AS "Mentor Last Name"
+  , users.first_name AS "Mentor First Name"
+  , LOWER(users.email1) AS "Mentor Email"
 
 
-FROM matviews.ss_cube
-  INNER JOIN sites USING (site_id)
-  INNER JOIN grade_levels USING (grade_level_id)
-  INNER JOIN students USING (student_id)
+FROM
+  matviews.ss_cube AS ss
+  INNER JOIN sites
+    USING (site_id)
+  INNER JOIN grade_levels
+    USING (grade_level_id)
+  INNER JOIN students
+    USING (student_id)
+  LEFT OUTER JOIN student_counselor_aff AS counselors
+    ON counselors.student_id = ss.student_id
+    AND counselors.start_date <= CURRENT_DATE
+    AND (counselors.end_date IS NULL OR counselors.end_date > CURRENT_DATE)
+  LEFT OUTER JOIN users
+    ON counselors.user_id = users.user_id
 
 
-WHERE leave_date >= CURRENT_DATE
-  AND site_name <> 'SPS Tour'
+
+WHERE
+      ss.leave_date > CURRENT_DATE
+  AND sites.site_name <> 'SPS Tour'
 
 
 ORDER BY
