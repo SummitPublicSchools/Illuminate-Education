ac4f9890b63b7dbc0ee6e8dbed0d7429d1f826fd
jostewart-summit <31998963+jostewart-summit@users.noreply.github.com>
2018-02-07 10:41:43 -0800

    Update illuminate_map_long.sql

--- illuminate_map_long.sql
+++ illuminate_map_long.sql
@@ -8,67 +8,68 @@ MAP Scores - Long Format
   in SY18 and SY17 he/she would have 12 rows returned as part of this query.
 ******************************************************************************/
 
-
 WITH map_sy18 AS (
  SELECT
    "nwea_2018_localStudentID" AS local_student_id
-   , "nwea_2018_SchoolName" AS school
-   , "nwea_2018_TermName" AS test_period
-   , "nwea_2018_Discipline" AS test_subject
-   , "nwea_2018_TestRITScore" AS test_score
-   , "nwea_2018_TestPercentile" AS test_percentile
-   , "nwea_2018_TypicalFallToWinterGrowth" AS typical_fall_winter_growth
-   , "nwea_2018_TypicalFallToSpringGrowth" AS typical_fall_spring_growth
+   ,"nwea_2018_SchoolName" AS school
+   ,"nwea_2018_TermName" AS test_period
+   ,"nwea_2018_Discipline" AS test_subject
+   ,"nwea_2018_TestRITScore" AS test_score
+   ,"nwea_2018_TestPercentile" AS test_percentile
+   ,"nwea_2018_TypicalFallToWinterGrowth" AS typical_fall_winter_growth
+   ,"nwea_2018_TypicalFallToSpringGrowth" AS typical_fall_spring_growth
  FROM national_assessments.nwea_2018
+ 
  WHERE
    --Exclude scores that NWEA does count for growth purposes
    "nwea_2018_GrowthMeasureYN" = 'TRUE' 
 ), map_sy17 AS (
  SELECT
    "nwea_2017_localStudentID" AS local_student_id
-   , "nwea_2017_SchoolName" AS school
-   , "nwea_2017_TermName" AS test_period
-   , "nwea_2017_Discipline" AS test_subject
-   , "nwea_2017_TestRITScore" AS test_score
-   , "nwea_2017_TestPercentile" AS test_percentile
-   , "nwea_2017_TypicalFallToWinterGrowth" AS typical_fall_winter_growth
-   , "nwea_2017_TypicalFallToSpringGrowth" AS typical_fall_spring_growth
+   ,"nwea_2017_SchoolName" AS school
+   ,"nwea_2017_TermName" AS test_period
+   ,"nwea_2017_Discipline" AS test_subject
+   ,"nwea_2017_TestRITScore" AS test_score
+   ,"nwea_2017_TestPercentile" AS test_percentile
+   ,"nwea_2017_TypicalFallToWinterGrowth" AS typical_fall_winter_growth
+   ,"nwea_2017_TypicalFallToSpringGrowth" AS typical_fall_spring_growth
  FROM national_assessments.nwea_2017
+ 
  WHERE
    --Exclude scores that NWEA does count for growth purposes
    "nwea_2017_GrowthMeasureYN" = 'TRUE' 
 ), map_all AS (
  SELECT
    local_student_id
-   , school
-   , test_period
-   , test_subject
-   , test_score
-   , test_percentile
-   , typical_fall_winter_growth
-   , typical_fall_spring_growth
+   ,school
+   ,test_period
+   ,test_subject
+   ,test_score
+   ,test_percentile
+   ,typical_fall_winter_growth
+   ,typical_fall_spring_growth
  FROM map_sy18
  
  UNION
  
  SELECT
    local_student_id
-   , school
-   , test_period
-   , test_subject
-   , test_score
-   , test_percentile
-   , typical_fall_winter_growth
-   , typical_fall_spring_growth
+   ,school
+   ,test_period
+   ,test_subject
+   ,test_score
+   ,test_percentile
+   ,typical_fall_winter_growth
+   ,typical_fall_spring_growth
  FROM map_sy17
 )
 SELECT
   local_student_id
-  , school
-  , test_period
-  , test_subject
-  , test_score
-  , test_percentile
-  , typical_fall_winter_growth
-  , typical_fall_spring_growth
+  ,school
+  ,test_period
+  ,test_subject
+  ,test_score
+  ,test_percentile
+  ,typical_fall_winter_growth
+  ,typical_fall_spring_growth
 FROM map_all
